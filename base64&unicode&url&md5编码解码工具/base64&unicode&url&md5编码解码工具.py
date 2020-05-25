6b0b335d746dd19367da7141801c163509b9cfae
WilliamL71Oi <37209194+WilliamL71Oi@users.noreply.github.com>
2020-05-24 21:13:47 -0400

    Update base64&unicode&url&md5编码解码工具.py

--- base64&unicode&url&md5编码解码工具/base64&unicode&url&md5编码解码工具.py
+++ base64&unicode&url&md5编码解码工具/base64&unicode&url&md5编码解码工具.py
@@ -74,15 +74,18 @@ def md5_en():
 # md5解密
 def md5_de():
     text = sys.argv[2].strip()
-    with open('60万条MD5密文.txt', 'r') as f:
-        for line in f:
-            if text == line.strip():
-                r = '\n' + text + ' MD5解密为：'
-                print(r, end='')
-                print(''.join(islice(f, 1)))
-                break
-        else:
-            print('\n您输入的MD5解密无匹配结果。')
+    if len(text) != 32:
+        print('\n请输入32位长度的MD5密文。')
+    else:
+        with open('60万条MD5密文.txt', 'r') as f:
+            for line in f:
+                if text == line.strip():
+                    r = '\n' + text + ' MD5解密为：'
+                    print(r, end='')
+                    print(''.join(islice(f, 1)))
+                    break
+            else:
+                print('\n您输入的MD5解密无匹配结果。')
 
 
 def main():
